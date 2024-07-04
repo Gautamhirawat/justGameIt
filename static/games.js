a8401a149f4948511ce233816dbf2f173d0440bf
Gautam Hirawat <113746508+Gautamhirawat@users.noreply.github.com>
2024-07-04 06:28:30 +0530

    update

---
+++ static/games.js
@@ -0,0 +1,120 @@
+
+
+document.getElementById('games').innerHTML =
+
+`
+
+
+    <link rel="stylesheet" href="static/games.css">    
+
+<div class="tiles">
+  <div class="col">
+    <a href="index.html"></a>
+    <a href="index.html"></a>
+    <a href="index.html"></a>
+    <a href="index.html"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+   <div class="col">
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <a href="#"></a>
+    <div class="box"></div>
+  </div>
+  
+</div>
+
+
+`;
---
+++ static/nav.js
@@ -0,0 +1,35 @@
+
+document.getElementById('nav_bar').innerHTML = `  
+
+    <link rel="stylesheet" href="static/slidebar.css">
+    <link rel="stylesheet" href="static/index.css">
+<nav class="navbar">
+        <ul class="navbar__menu">
+          <li class="navbar__item">
+            <a href="index.html" class="navbar__link"><i data-feather="home"></i><span>Home</span></a>
+          </li>
+          <li class="navbar__item">
+            <a href="#" class="navbar__link"><i data-feather="message-square"></i><span>Notification</span></a>        
+          </li>
+          <li class="navbar__item">
+            <a href="#" class="navbar__link"><i data-feather="users"></i><span>Login</span></a>        
+          </li>
+          <li class="navbar__item">
+            <a href="../games.html" class="navbar__link"><i data-feather="play"></i><span>Games</span></a>        
+          </li>
+          <li class="navbar__item">
+            <a href="#Resources" class="navbar__link"><i data-feather="archive"></i><span>Resources</span></a>        
+          </li>
+          <li class="navbar__item">
+            <a href="#" class="navbar__link"><i data-feather="help-circle"></i><span>Help</span></a>        
+          </li>
+          <li class="navbar__item">
+            <a href="#" class="navbar__link"><i data-feather="settings"></i><span>Settings</span></a>        
+          </li>
+        </ul>
+      </nav>
+
+
+
+
+`;
